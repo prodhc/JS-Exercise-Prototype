e61b1fba7deb71e93a5f17347310e3aa826a9384
prodhc <zionywood@gmail.com>
2021-03-24 13:09:00 -0700

    Finished 3/4

--- index.js
+++ index.js
@@ -39,11 +39,32 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
+
+ function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []; 
+  this.toString = () => { return `${this.name}, ${this.age}`}; 
+ };
+
+ Person.prototype.eat = function (someFood) {
+    if (this.stomach.length <= 10 ) {
+      this.stomach.push(someFood);
+    }
     
-  }
+  };
+
+Person.prototype.poop = function () {
+    this.stomach = [];
+}
+
+Person.prototype.toString = function () {
+  return `${this.name}, ${this.age}`;
+};
  
- 
+
+ const secoya = new Person('Secoya', 19);
+console.log(secoya);
 
   
   
@@ -56,6 +77,7 @@ function Airplane(name) {
           + should initialize with an `tank` at 0
           + should initialize with an `odometer` at 0
       - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+
       - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
           + Should cause the `odometer` to go up.
           + Should cause the the `tank` to go down taking `milesPerGallon` into account.
@@ -63,9 +85,18 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
-  }
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  };
+
+  Car.prototype.fill = function (gallons) {
+    this.tank += gallons;
+  };
+
+  const truck = new Car("ford", 100);
   
   
   /*
@@ -75,9 +106,17 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+ function Baby(name, age, favoriteToy) {
+   this.name = name;
+   this.age = age;
+   this.favoriteToy = favoriteToy;
+  };
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function () {
+    return `Playing with ${this.favoriteToy}`;
+  };
  
   
   /* 
